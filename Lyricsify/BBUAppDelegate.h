0f392f0305bc9dd774bab0d919b25d0e65aa2734
Boris Bügling <boris@icculus.org>
2013-05-03 11:33:26 +0200

    Initial Commit

---
+++ Lyricsify/BBUAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BBUAppDelegate.h
+//  Lyricsify
+//
+//  Created by Boris Bügling on 03.05.13.
+//  Copyright (c) 2013 Boris Bügling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BBUAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
